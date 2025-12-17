7fbac465522a031e88646abf42394a64d3af7191
jogaila06 <jogaila2006@gmail.com>
2025-12-17 19:53:56 +0000

    Add files via upload

---
+++ Lee & Jogaila Webieeeeee/JavaScript/fallDown.js
@@ -0,0 +1,41 @@
+let isMovedDown = false;
+
+function leeDropDown(){
+   const element = document.getElementsByClassName("title")[0];
+   const navBar = document.getElementsByClassName("navBar")[0];
+
+    if (isMovedDown) {
+        navBar.classList.remove("active");
+        setTimeout(() => {
+        document.querySelectorAll(".lee > div").forEach(el => {el.style.display = "none";}); 
+        document.querySelectorAll(".jogaila > div").forEach(el => {el.style.display = "none";});
+        }, 1000)
+        
+        
+    } else {
+        navBar.classList.add("active");
+        document.querySelectorAll(".lee > div").forEach(el => {el.style.display = "flex";});
+    }
+
+    isMovedDown = !isMovedDown;
+}
+
+function jogailaDropDown(){
+   const element = document.getElementsByClassName("title")[0];
+   const navBar = document.getElementsByClassName("navBar")[0];
+
+    if (isMovedDown) {
+        navBar.classList.remove("active");
+        setTimeout(() => {
+        document.querySelectorAll(".jogaila > div").forEach(el => {el.style.display = "none";});
+        document.querySelectorAll(".lee > div").forEach(el => {el.style.display = "none";});
+        }, 1000)
+        
+    } else {
+        navBar.classList.add("active");
+        document.querySelectorAll(".jogaila > div").forEach(el => {el.style.display = "flex";});
+    }
+
+    isMovedDown = !isMovedDown;
+}
+
